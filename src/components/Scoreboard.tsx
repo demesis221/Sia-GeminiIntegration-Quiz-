61e239ec59863cb54363902752358d56b82c5489
demesis221 <rideouts199@gmail.com>
2025-10-29 18:33:18 +0800

    Fix build errors: remove unused imports and fix syntax errors

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { getProgress, getQuizResults } from '../utils/storage';
 
 const Scoreboard: React.FC = () => {
